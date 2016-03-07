c90fd6d85884e7bbf79cda77dda1f00cf428cf16
lilach <lilachvvc@gmail.com>
2016-03-07 21:18:06 +0000

    Books home page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ActiveRecord::Base
     belongs_to :category
+    
+    #returns most recent books
+    def self.recent(limit = 5)
+        @recent_books = Book.order('created_at desc').limit(limit)
+    end
 end
